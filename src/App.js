802083475711880f1e0d58e44693fbb99815ab60
bigkon <ggbigkon@gmail.com>
2021-03-24 00:50:20 +0200

    Finished frontend part & added requirements.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,55 @@ import 'regenerator-runtime/runtime'
 import React from 'react'
 import { login, logout } from './utils'
 import './global.css'
+import {BN} from 'bn.js'
 
 import getConfig from './config'
 const { networkId } = getConfig(process.env.NODE_ENV || 'development')
 
+function ConvertToYoctoNear(amount) {
+    return new BN(Math.round(amount * 100000000)).mul(new BN('10000000000000000')).toString();
+}
+
 export default function App() {
   // use React Hooks to store greeting in component state
-  const [greeting, set_greeting] = React.useState()
+  const [greeting, set_greeting] = React.useState();
+  const [accounts, setAccounts] = React.useState();
+  const [total, setTotal] = React.useState(0);
 
   // when the user has not yet interacted with the form, disable the button
   const [buttonDisabled, setButtonDisabled] = React.useState(true)
 
   // after submitting the form, we want to show Notification
   const [showNotification, setShowNotification] = React.useState(false)
+
+  let parseAccounts = function(input) {
+    const re = RegExp(/^(.*)\s(\d+\.?\d*)$/);
+    let accounts = {}, total=0;
+    let lines = input.split('\n');
+    for (let i=0; i<lines.length; i++) {
+        let line = lines[i];
+        let spaceIndex = line.lastIndexOf(' ');
+        if (spaceIndex < 1) {
+            continue;
+        }
+        let name = line.substring(0, spaceIndex);
+        let amount = line.substring(spaceIndex, line.length);
+        if (!name || !amount || !Number(amount)) {
+            continue;
+        }
+        amount = Number(amount);
+        total += amount;
+        if (accounts.hasOwnProperty(name)) {
+            accounts[name] += amount;
+        } else {
+            accounts[name] = amount;
+        }
+    }
+    setTotal(total);
+    setAccounts(accounts);
+    setButtonDisabled(false);
+    console.log(accounts);
+  }
 
   // The useEffect hook can be used to fire side-effects during render
   // Learn more: https://reactjs.org/docs/hooks-intro.html
@@ -95,11 +131,12 @@ export default function App() {
           fieldset.disabled = true
 
           try {
+            let operations = Object.keys(accounts).reduce(function(acc, cur) {
+                acc.push({account_id: cur, amount: ConvertToYoctoNear(accounts[cur])});
+                return acc;
+            }, []);
             // make an update call to the smart contract
-            await window.contract.set_greeting({
-              // pass the value that the user entered in the greeting field
-              message: newGreeting
-            })
+            await window.contract.send({operations: operations}, 300000000000000, ConvertToYoctoNear(total))
           } catch (e) {
             alert(
               'Something went wrong! ' +
@@ -136,18 +173,18 @@ export default function App() {
               Change greeting
             </label>
             <div style={{ display: 'flex' }}>
-              <input
+              <textarea
                 autoComplete="off"
                 defaultValue={greeting}
                 id="greeting"
-                onChange={e => setButtonDisabled(e.target.value === greeting)}
+                onChange={e => parseAccounts(e.target.value)}
                 style={{ flex: 1 }}
               />
               <button
                 disabled={buttonDisabled}
                 style={{ borderRadius: '0 5px 5px 0' }}
               >
-                Save
+                Send
               </button>
             </div>
           </fieldset>
